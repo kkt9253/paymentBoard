275a300f9ecd8f54b6335c80b9a0f5d7c247ca85
kkt9253 <kkt9253@naver.com>
2025-01-11 17:22:08 +0900

    DOCS : 초기 설정

---
+++ src/main/java/graduationtoyproject/paymentboard/PaymentBoardApplication.java
@@ -0,0 +1,13 @@
+package graduationtoyproject.paymentboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/graduationtoyproject/paymentboard/PaymentBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package graduationtoyproject.paymentboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
